6cd33ed4eaaf119fec61c8b708050be9ee7e6900
jinbixiaopaigu <lkjhgfdsa12q@gmail.com>
2024-05-16 08:41:23 +0800

    变更

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -12,6 +15,7 @@ import java.util.List;
 
 @Controller
 public class StudentController {
+    private static final Logger log = LoggerFactory.getLogger(StudentController.class);
     private static List<Student> students=new ArrayList<>();
     static {
         Student s = new Student("jinbi",1);
@@ -23,5 +27,13 @@ public class StudentController {
     public String list(ModelMap modelMap){
         modelMap.put("students",students);
         return "list";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String delete(@PathVariable String id){
+//        System.out.println("id:"+id);
+        log.warn("id:"+(Integer.parseInt(id)-1)+"");
+        students.remove(Integer.parseInt(id)-1);
+        return "forward:/list";
     }
 }
