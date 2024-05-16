18e850d9dd18784bde4f7809955dbac99dbdf0a4
jinbixiaopaigu <lkjhgfdsa12q@gmail.com>
2024-05-16 10:25:54 +0800

    数据结构改为map

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -12,30 +12,30 @@ import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class StudentController {
     private static final Logger log = LoggerFactory.getLogger(StudentController.class);
-    private static List<Student> students=new ArrayList<>();
+    private static Map<String,Student> students=new HashMap<>();
     static {
         Student s = new Student("jinbi","1");
-        students.add(s);
-        students.add(new Student("yininbi","2"));
+        students.put(s.getId(),s);
+        students.put("2",new Student("yininbi","2"));
     }
 
     @RequestMapping("list")
     public String list(ModelMap modelMap){
-        modelMap.put("students",students);
+        modelMap.put("students",students.values());
         return "list";
     }
 
     @RequestMapping("/delete/{id}")
     public String delete(@PathVariable String id){
-//        System.out.println("id:"+id);
         log.warn("id:"+(Integer.parseInt(id)-1)+"");
-//        students.remove(Integer.parseInt(id)-1);
-        students.removeIf(student -> Integer.parseInt(student.getId()) == (Integer.parseInt(id)));
+        students.remove(id);
         return "redirect:/list";
     }
 
@@ -46,15 +46,16 @@ public class StudentController {
 
     @RequestMapping("/save")
     public String save(Student student ){
-        log.warn("student:"+student.getName());
-        student.setId(String.valueOf(students.size()+1));
-        students.add(student);
+        if(student.getId()==null){
+            student.setId(String.valueOf(students.size()+1));
+        }
+        students.put(student.getId(),student);
         return "redirect:/list";
     }
 
     @RequestMapping("/update/{id}")
     public String update(@PathVariable String id, ModelMap map){
-        Student student=students.get(Integer.parseInt(id));
+        Student student=students.get(id);
         map.put("student",student);
         return "update";
     }
